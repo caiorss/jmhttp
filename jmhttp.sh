bb664945c257a8450b815e020348dce9567e84d3
CaioRS <caiorss.rodrigues@gmail.com>
2018-06-19 06:41:01 -0300

    Remove old files and update .gitignore.

--- jmhttp.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-#
-# Shell script wrapper to run app during development.
-# Always run $ make, before running this script.
-#
-jarget exec javax.jmdns/jmdns/3.4.1 -- scala bin/jmhttp.jar "$@"
